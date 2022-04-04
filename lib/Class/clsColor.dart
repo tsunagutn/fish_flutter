2a1fd7bccec306468c0a2f6cf4036b51fc2205f3
nagamine <toru.nagamine@tsunagu.ltd>
2022-04-04 19:43:58 +0900

    色コードクラス追加

---
+++ lib/Class/clsColor.dart
@@ -0,0 +1,39 @@
+
+//色コード関連操作クラス
+import 'package:flutter/material.dart';
+
+class clsColor {
+  //色を色コード文字列で指定する
+  static Color getColorFromHex(String hexColor) {
+    hexColor = hexColor.toUpperCase().replaceAll('#', '');
+    if (hexColor.length == 6) {
+      hexColor = 'FF' + hexColor;
+    }
+    return Color(int.parse(hexColor, radix: 16));
+  }
+
+  //色を値範囲によって可変にする
+  static Color getColorRange(
+      Color startColor, Color endColor, double val, double maxVal) {
+    var diffVal = (val / maxVal);
+    //R
+    var r =
+        startColor.red + ((endColor.red - startColor.red) * diffVal).floor();
+    //G
+    var g = startColor.green +
+        ((endColor.green - startColor.green) * diffVal).floor();
+    //B
+    var b =
+        startColor.blue + ((endColor.blue - startColor.blue) * diffVal).floor();
+    return getColorFromHex(r.toRadixString(16).padLeft(2, "0") +
+        g.toRadixString(16).padLeft(2, "0") +
+        b.toRadixString(16).padLeft(2, "0"));
+  }
+
+  //割合によって色を返す
+  static Color getRaitoColor(double raito) {
+    if (raito > 0.7) return Colors.green;
+    if (raito > 0.3) return Colors.amber;
+    return Colors.red;
+  }
+}
