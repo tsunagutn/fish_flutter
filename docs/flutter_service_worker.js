764971f382a7548e48a4b7cb204276d649d1f30a
nagamine <toru.nagamine@tsunagu.ltd>
2022-06-01 10:39:49 +0900

    20220601

--- docs/flutter_service_worker.js
+++ docs/flutter_service_worker.js
@@ -135,8 +135,8 @@ const RESOURCES = {
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
-"index.html": "3713229d34428aff656ff5521789f586",
-"/": "3713229d34428aff656ff5521789f586",
+"index.html": "c09e069247b288ad573ba28f733610f8",
+"/": "c09e069247b288ad573ba28f733610f8",
 "main.dart.js": "f3c15a7169b35822a91bf8665478f8fc",
 "manifest.json": "c634c13abd31b54604e8fb848d0a85ec",
 "version.json": "2625bcf7276b52a536463391f68a7f73"
